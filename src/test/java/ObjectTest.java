f5793b48aad2b4a9e9e60c6d5967ca5aca764f34
gaeun <knospe1@naver.com>
2022-06-15 00:13:30 +0900

    test: Object의 hashCode와 String hashCode 테스트코드 작성

--- src/test/java/ObjectTest.java
+++ src/test/java/ObjectTest.java
@@ -1,15 +1,26 @@
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 public class ObjectTest {
 
-    @Test
-    void equals() {
-        Value v1 = new Value(10);
-        Value v2 = new Value(10);
+    private Value v1;
+    private Value v2;
 
+    @BeforeEach
+    void setUp() {
+        v1 = new Value(10);
+        v2 = new Value(10);
+    }
+
+    @Test
+    void equalsTest() {
         Assertions.assertThat(v1.equals(v2)).isEqualTo(false);
     }
 
+    @Test
+    void hashCodeTest() {
+        Assertions.assertThat(System.identityHashCode(v1)).isNotEqualTo(System.identityHashCode(v2));
+    }
 }
--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,13 +1,25 @@
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
 
-    @Test
-    void equals() {
-        String str1 = "abc";
-        String str2 = "abc";
+    private String str1;
+    private String str2;
 
+    @BeforeEach
+    void setUp() {
+        str1 = "abc";
+        str2 = "abc";
+    }
+
+    @Test
+    void equalsTest() {
         Assertions.assertThat(str1.equals(str2)).isEqualTo(true);
+    }
+
+    @Test
+    void hashCodeTest() {
+        Assertions.assertThat(str1.hashCode()).isEqualTo(str2.hashCode());
     }
 }
